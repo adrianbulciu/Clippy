1a316bbf9f127950440ce5921427df9389bd39bd
Bulciu Adrian <bulciu.adrian@gmail.com>
2025-12-25 09:02:24 +0200

    Initial Commit

---
+++ Clippy/ClippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClippyApp.swift
+//  Clippy
+//
+//  Created by Adrian Bulciu on 25.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ClippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
