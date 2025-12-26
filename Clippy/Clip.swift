c377d90d41e7c009a772c355bee21164411813db
Bulciu Adrian <bulciu.adrian@gmail.com>
2025-12-26 10:09:29 +0200

    init

---
+++ Clippy/Clip.swift
@@ -0,0 +1,8 @@
+//
+//  Clip.swift
+//  Clippy
+//
+//  Created by Adrian Bulciu on 26.12.2025.
+//
+
+import Foundation
---
+++ Clippy/ClipView.swift
@@ -0,0 +1,18 @@
+//
+//  ClipView.swift
+//  Clippy
+//
+//  Created by Adrian Bulciu on 26.12.2025.
+//
+
+import SwiftUI
+
+struct ClipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClipView()
+}
