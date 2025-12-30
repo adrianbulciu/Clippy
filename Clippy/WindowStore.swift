cdc36d3c6975826eade4f8893d8b34270f78e920
Bulciu Adrian <bulciu.adrian@gmail.com>
2025-12-30 13:23:36 +0200

    separate window accessor

--- Clippy/WindowStore.swift
+++ Clippy/WindowStore.swift
@@ -12,18 +12,3 @@ final class WindowStore {
     static let shared = WindowStore()
     weak var clipsWindow: NSWindow?
 }
-
-struct WindowAccessor: NSViewRepresentable {
-    var callback: (NSWindow?) -> Void
-    
-    func makeNSView(context: Context) -> NSView {
-        let view = NSView()
-        DispatchQueue.main.async {
-            self.callback(view.window)
-        }
-        return view
-    }
-    
-    func updateNSView(_ nsView: NSView, context: Context) {
-    }
-}
